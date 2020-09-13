b0170737bdeba35a98a07af8c46e6750a7f522b4
phoenix-wuzf <wzf@wuzufei.cn>
2020-09-13 15:33:00 +0800

    commit code for contorus

--- src/com/wuzufei/sudo/Main.java
+++ src/com/wuzufei/sudo/Main.java
@@ -1,13 +1,13 @@
 package com.wuzufei.sudo;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.core.Point;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.awt.*;
+import java.awt.font.ImageGraphicAttribute;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,23 +21,39 @@ public class Main {
         Mat src = Imgcodecs.imread("C:\\\\Users\\\\phoenix-Wu\\\\Desktop\\\\test.png");
         //2 图片灰度化
         Mat gary = new Mat();
-        Imgproc.cvtColor(src, gary, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.cvtColor(src, gary, Imgproc.COLOR_BGR2GRAY);
         //3 图像边缘处理
-        Mat edges = new Mat();
-        Imgproc.Canny(gary, edges, 200, 500, 3, false);
+        Mat thresh = new Mat();
+        Mat dilate = gary.clone();
+        //Imgproc.Canny(gary, edges, 200, 500, 3, false);
+        Imgproc.threshold(gary, thresh, 200, 255, 1);
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS, new Size(5,5));
+        Imgproc.dilate(thresh, dilate, kernel);
         //4 发现轮廓
 
         List<MatOfPoint> list = new ArrayList<MatOfPoint>();
         Mat hierarchy = new Mat();
-        Imgproc.findContours(edges, list, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        //5 绘制轮廓
+        Imgproc.findContours(dilate, list, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        int m = 0;
         for (int i = 0, len = list.size(); i < len; i++) {
-            if (i % 2 != 0) {
-                Imgproc.drawContours(src, list, i, new Scalar(0, 255, 0), 1, Imgproc.LINE_AA);
+            //System.out.println(hierarchy.get(0, i));
+//            if ((hierarchy.get(0, i))[3] == 0 && (hierarchy.get(0, i))[2] > 0) {
+//                m++;
+//                int idx = (int) (hierarchy.get(0, i))[0];
+//                Imgproc.drawContours(src, list, idx, new Scalar(0, 0, 255), 2, Imgproc.LINE_AA);
+//            }
+            if ((hierarchy.get(0, i))[3] == 0) {
+                if ((hierarchy.get(0, i))[2] > 0) {
+                    m++;
+                    int idx = (int) (hierarchy.get(0, i))[2];
+                    //Imgproc.drawContours(src, list, i, new Scalar(0, 0, 255), 2, Imgproc.LINE_AA);
+                    Rect area = Imgproc.boundingRect(list.get(idx));
+                    Imgproc.rectangle(src, area, new Scalar(0, 0, 255), 2);
+                }
             }
         }
         System.out.println(list.size());
+        System.out.println(m);
         HighGui.imshow("111", src);
         HighGui.waitKey(0);
 
