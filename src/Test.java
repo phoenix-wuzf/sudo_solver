f47949332286dbdfd00183320a299263ba0841d3
phoenix-wuzf <wzf@wuzufei.cn>
2020-09-20 23:53:07 +0800

    just for data

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Queue;
-
-public class Test {
-    public static void main(String[] args) {
-        ArrayList<Integer> ret = new ArrayList<>();
-
-    }
-}
